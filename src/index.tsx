148bc6862b638698ec74e78a720232256f6611da
Astronawta <thespacewalkerbr@gmail.com>
2024-05-13 10:35:19 -0300

    att

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { TextInput, Text, View } from "react-native";
+import { styles } from './styles';
+
+export function SignIn() {
+    return(
+        <View style={styles.container}>
+            <Text>Hello World</Text>
+
+
+
+
+
+            <TextInput />
+        </View>
+    );
+}
